4ad075908228aa65cb9c531e51bf410dbad622dc
Vitor Soares <vitorvieirasoares@gmail.com>
2018-01-04 16:56:41 +0000

    Initial commit

---
+++ app/src/main/java/com/example/vitorsoares/computacaomovel/LoginPage.java
@@ -0,0 +1,13 @@
+package com.example.vitorsoares.computacaomovel;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LoginPage extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_page);
+    }
+}
